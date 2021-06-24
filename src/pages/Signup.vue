ebe4984c03433b5f232a87efa9f852385ac52654
rukkiecodes <rukkiecodes@gmail.com>
2021-06-24 22:44:20 +0100

    added vue-country-code-select

--- src/pages/Signup.vue
+++ src/pages/Signup.vue
@@ -2,27 +2,61 @@
   <div class="signup">
     <div class="hero">
       <q-img
-      :src="phone"
-      spinner-color="white"
-      class="phone"
-    />
+        :src="phone"
+        spinner-color="white"
+        class="phone"
+      />
 
-    <div class="social">
-      <p>Sign up with</p>
+      <div class="social">
+        <p>Sign up with</p>
 
-      <div class="buttons">
-         <q-btn unelevated class="button" color="primary" icon="mdi-linkedin" />
-         <q-btn unelevated class="button q-mx-lg" color="primary" icon="mdi-facebook" />
-         <q-btn unelevated class="button" color="primary" icon="mdi-github" />
+        <div class="buttons">
+          <q-btn
+            unelevated
+            class="button"
+            color="primary"
+            icon="mdi-linkedin"
+          />
+          <q-btn
+            unelevated
+            class="button q-mx-lg"
+            color="primary"
+            icon="mdi-facebook"
+          />
+          <q-btn
+            unelevated
+            class="button"
+            color="primary"
+            icon="mdi-github"
+          />
+        </div>
       </div>
     </div>
+    <div class="form">
+      <div class="head">
+        <q-img
+          :src="logo"
+          spinner-color="white"
+          class="logo"
+        />
+        <p class="text">Sign up</p>
+        <p class="small_text">Enter details to sign up</p>
+
+        <div class="inputs">
+          <div class="input">
+            <input type="text" placeholder="Full name">
+          </div>
+          <div class="input">
+            <input type="text" placeholder="E-mail">
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="form"></div>
   </div>
 </template>
 
 <script>
-export default{
+export default {
   name: 'PageIndex',
   data: () => ({
     phone: require("../assets/trust/phone1.png"),
