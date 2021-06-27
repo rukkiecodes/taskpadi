67a545dd0afa8d77b9152d910d5318db74294543
rukkiecodes <rukkiecodes@gmail.com>
2021-06-27 01:12:20 +0100

    layed out section 2

--- src/components/home/Section2.vue
+++ src/components/home/Section2.vue
@@ -3,6 +3,38 @@
     <div class="head_text">
       <p>Why use Trustpaddi</p>
     </div>
+    <div class="cards">
+      <div class="card card_one">
+        <div class="head">
+          <img
+            src="../../assets/trust/homepage icons/Group 129.svg"
+            alt=""
+          >
+          <p>Safe</p>
+        </div>
+        <p>Lorem ipsum dolor amet consectrur disciping sit amet consectrur.</p>
+      </div>
+      <div class="card card_two">
+        <div class="head">
+          <img
+            src="../../assets/trust/homepage icons/Group 130.svg"
+            alt=""
+          >
+          <p>Secure</p>
+        </div>
+        <p>Lorem ipsum dolor amet consectrur disciping sit amet consectrur.</p>
+      </div>
+      <div class="card card_three">
+        <div class="head">
+          <img
+            src="../../assets/trust/homepage icons/Group 131.svg"
+            alt=""
+          >
+          <p>Fast</p>
+        </div>
+        <p>Lorem ipsum dolor amet consectrur disciping sit amet consectrur.</p>
+      </div>
+    </div>
   </v-container>
 </template>
 
