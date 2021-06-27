f6b58f95be27ae8b45d3034992f29274e573961d
rukkiecodes <rukkiecodes@gmail.com>
2021-06-27 23:32:55 +0100

    layedout who can use component

--- src/components/home/Who_can_use.vue
+++ src/components/home/Who_can_use.vue
@@ -1,7 +1,19 @@
 <template>
   <v-container class="who_can_use">
-    <div class="head_text">
-      <p>Who can use Trustpaddi</p>
+    <div class="who_can_use_head_text">
+      <p class="p1">Who can use Trustpaddi</p>
+    </div>
+
+    <div class="who_can_use_cards">
+      <div v-for="(card, index) in cards" :key="index" class="who_can_use_cards_card rounded-xl">
+        <div class="img">
+          <img :src="card.image" :alt="card.head" />
+        </div>
+        <div class="texts">
+          <p class="p1 text-h6" v-text="card.head" />
+          <p class="p2 text-body-2" v-text="card.body" />
+        </div>
+      </div>
     </div>
   </v-container>
 </template>
@@ -9,7 +21,26 @@
 <script>
 export default {
   data: () => ({
-    cards: [],
+    cards: [
+      {
+        image: require("../../assets/trust/homepage icons/Group 139.svg"),
+        head: "Lorem ipsum",
+        body:
+          "Lorem ipsum dolor sit amet lorem consectrur disciping Lorem ipsum dolor sit amet consectrur.",
+      },
+      {
+        image: require("../../assets/trust/homepage icons/Group 140.svg"),
+        head: "Lorem ipsum",
+        body:
+          "Lorem ipsum dolor sit amet lorem consectrur disciping Lorem ipsum dolor sit amet consectrur.",
+      },
+      {
+        image: require("../../assets/trust/homepage icons/Group 141.svg"),
+        head: "Lorem ipsum",
+        body:
+          "Lorem ipsum dolor sit amet lorem consectrur disciping Lorem ipsum dolor sit amet consectrur.",
+      },
+    ],
   }),
 };
 </script>
