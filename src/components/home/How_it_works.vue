1a8ef60be9f9f96b3d38f0cc54c1300fef3d6a16
rukkiecodes <rukkiecodes@gmail.com>
2021-06-27 22:16:12 +0100

    layedout how it works

--- src/components/home/How_it_works.vue
+++ src/components/home/How_it_works.vue
@@ -1,8 +1,51 @@
 <template>
-  <v-container>
+  <v-container class="how_it_works">
     <div class="head_text">
       <p>Why use Trustpaddi</p>
     </div>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo nam eos, aliquid autem illo officia, accusamus nihil reprehenderit fugit adipisci alias repellat ad optio culpa! Harum, sint veniam. A, nobis.
+    <div class="how_it_works_cards">
+      <div v-for="(card, index) in cards" :key="index" class="how_it_works_card">
+        <img :src="card.image" alt="">
+        <span v-text="card.text" />
+      </div>
+    </div>
   </v-container>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    cards: [
+      {
+        image: require("../../assets/trust/homepage icons/Group 112.svg"),
+        text:
+          "Lorem ipsum dolor sit amet consectrur disciping Lorem ipsum dolor sit amet consectrur. Lorem ipsum dolor sit amet consectrur.",
+      },
+      {
+        image: require("../../assets/trust/homepage icons/Group 113.svg"),
+        text:
+          "Lorem ipsum dolor sit amet consectrur disciping Lorem ipsum dolor sit amet consectrur. Lorem ipsum dolor sit amet consectrur.",
+      },
+      {
+        image: require("../../assets/trust/homepage icons/Group 115.svg"),
+        text:
+          "Lorem ipsum dolor sit amet consectrur disciping Lorem ipsum dolor sit amet consectrur. Lorem ipsum dolor sit amet consectrur.",
+      },
+      {
+        image: require("../../assets/trust/homepage icons/Group 117.svg"),
+        text:
+          "Lorem ipsum dolor sit amet consectrur disciping Lorem ipsum dolor sit amet consectrur. Lorem ipsum dolor sit amet consectrur.",
+      },
+      {
+        image: require("../../assets/trust/homepage icons/Group 118.svg"),
+        text:
+          "Lorem ipsum dolor sit amet consectrur disciping Lorem ipsum dolor sit amet consectrur. Lorem ipsum dolor sit amet consectrur.",
+      },
+    ],
+  }),
+};
+</script>
+
+<style scoped>
+@import url(../../assets/styles/how_it_works.css);
+</style>
