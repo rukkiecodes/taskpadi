c891770ee1be21d3029b0ac4f8feb9d0caae913d
rukkiecodes <rukkiecodes@gmail.com>
2021-06-28 19:47:52 +0100

    layed out testimonials

--- src/components/home/Testimonials.vue
+++ src/components/home/Testimonials.vue
@@ -1,24 +1,74 @@
 <template>
   <v-container class="testimonials">
     <div class="slide">
-      <VueSlickCarousel class="slide_slick" :slidesToShow="3" :arrows="true" :dots="true">
-        <div class="card">1</div>
-        <div class="card">2</div>
-        <div class="card">3</div>
-      </VueSlickCarousel>
+      <v-sheet class="mx-auto sheet" elevation="0">
+        <v-slide-group
+          v-model="model"
+        >
+          <v-slide-item v-for="(card, index) in cards" :key="index">
+            <v-card width="320" flat class="ma-4 sheet_card rounded-xl px-6 py-12">
+              <v-card-text class="ma-0 pa-0">
+                <div class="head mb-5">
+                  <div class="icon">
+                    <v-icon size="80">{{ card.icon }}</v-icon>
+                  </div>
+                </div>
+              </v-card-text>
+              <v-card-text class="ma-0 pa-0 texts">
+                <p class="p1 text-h6 font-weight-bold">{{ card.title }}</p>
+                <p class="p2 text-body-2">{{ card.text }}</p>
+              </v-card-text>
+            </v-card>
+          </v-slide-item>
+        </v-slide-group>
+      </v-sheet>
     </div>
   </v-container>
 </template>
 
 <script>
-import VueSlickCarousel from 'vue-slick-carousel'
-  import 'vue-slick-carousel/dist/vue-slick-carousel.css'
-  // optional style for arrows & dots
-  import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
-
 export default {
-  name: "MyComponent",
-  components: { VueSlickCarousel },
+  data: () => ({
+    cards: [
+      {
+        icon: "mdi-account",
+        title: "Victory Roberts",
+        text: `Lorem ipsum dolor sit amet lorem consectrur disciping Lorem ipsum
+              dolor sit amet consectrur. Lorem ipsum dolor sit amet lorem
+              consectrur disciping Lorem ipsum dolor sit amet consectrur.Lorem
+              ipsum dolor sit amet lorem consectrur disciping Lorem ipsum dolor
+              sit amet consectrur.`,
+      },
+      {
+        icon: "mdi-account",
+        title: "Victory Roberts",
+        text: `Lorem ipsum dolor sit amet lorem consectrur disciping Lorem ipsum
+              dolor sit amet consectrur. Lorem ipsum dolor sit amet lorem
+              consectrur disciping Lorem ipsum dolor sit amet consectrur.Lorem
+              ipsum dolor sit amet lorem consectrur disciping Lorem ipsum dolor
+              sit amet consectrur.`,
+      },
+      {
+        icon: "mdi-account",
+        title: "Victory Roberts",
+        text: `Lorem ipsum dolor sit amet lorem consectrur disciping Lorem ipsum
+              dolor sit amet consectrur. Lorem ipsum dolor sit amet lorem
+              consectrur disciping Lorem ipsum dolor sit amet consectrur.Lorem
+              ipsum dolor sit amet lorem consectrur disciping Lorem ipsum dolor
+              sit amet consectrur.`,
+      },
+      {
+        icon: "mdi-account",
+        title: "Victory Roberts",
+        text: `Lorem ipsum dolor sit amet lorem consectrur disciping Lorem ipsum
+              dolor sit amet consectrur. Lorem ipsum dolor sit amet lorem
+              consectrur disciping Lorem ipsum dolor sit amet consectrur.Lorem
+              ipsum dolor sit amet lorem consectrur disciping Lorem ipsum dolor
+              sit amet consectrur.`,
+      },
+    ],
+    model: null,
+  }),
 };
 </script>
 
