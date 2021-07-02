ef8f6b58f4ca08202c4b6b8435e7a61d3f27e539
rukkiecodes <rukkiecodes@gmail.com>
2021-07-02 21:31:22 +0100

    made changes to read more

--- src/components/home/Read_more.vue
+++ src/components/home/Read_more.vue
@@ -9,46 +9,17 @@
       </div>
 
       <div class="read_more_cards">
-        <div class="read_more_card py-5 px-5 rounded-lg">
+        <div
+          v-for="(card, index) in cards"
+          :key="index"
+          class="read_more_card py-5 px-5 rounded-lg"
+        >
           <div class="card_head mt-3 py-2">
             <div class="read_more_card_avatar" color="#EFF0F7">
-              <img
-                src="../../assets/trust/sad-emoticon-square-face.png"
-                alt=""
-              />
+              <img :src="card.image" alt="" />
             </div>
           </div>
-          <p class="mt-4 text-body-2 pt-3">
-            We know how Devastating it can be losing money to an online
-            fraudster, and if not Cash On Delivery, we're sure you won't make
-            any purchases.
-          </p>
-        </div>
-        <div class="read_more_card py-5 px-5 rounded-lg">
-          <div class="card_head mt-3 py-2">
-            <div class="read_more_card_avatar" color="#EFF0F7">
-              <img src="../../assets/trust/tap.png" alt="" />
-            </div>
-          </div>
-          <p class="mt-4 text-body-2 pt-3">
-            For business owners and service providers, We also know how hard it
-            can be getting potential customers to trust the genuineness of your
-            upcoming brand online, on the other hand Cash on delivery isn't
-            always a convenient option for you.
-          </p>
-        </div>
-        <div class="read_more_card py-5 px-5 rounded-lg">
-          <div class="card_head mt-3 py-2">
-            <div class="read_more_card_avatar" color="#EFF0F7">
-              <img src="../../assets/trust/cooperation.png" alt="" />
-            </div>
-          </div>
-          <p class="mt-4 text-body-2 pt-3">
-            Therefore, It all boils down to the issue of "trust" and this is
-            what TrustPaddi is all about, bridging the trust gap between
-            potential customers, service providers & businesses online while
-            protecting there various interest.
-          </p>
+          <p class="mt-4 text-body-1 pt-3" v-text="card.text" />
         </div>
       </div>
     </div>
@@ -58,25 +29,21 @@
 <script>
 export default {
   data: () => ({
-    dialog: false,
-    isMobile: false,
+    cards: [
+      {
+        image: require("../../assets/trust/sad-emoticon-square-face.png"),
+        text: `We know how Devastating it can be losing money to an online fraudster, and if not Cash On Delivery, we're sure you won't make any purchases.`,
+      },
+      {
+        image: require("../../assets/trust/tap.png"),
+        text: `For business owners and service providers, We also know how hard it can be getting potential customers to trust the genuineness of your upcoming brand online, on the other hand Cash on delivery isn't always a convenient option for you.`,
+      },
+      {
+        image: require("../../assets/trust/cooperation.png"),
+        text: `Therefore, It all boils down to the issue of "trust" and this is what TrustPaddi is all about, bridging the trust gap between potential customers, service providers & businesses online while protecting there various interest.`,
+      },
+    ],
   }),
-  beforeDestroy() {
-    if (typeof window === "undefined") return;
-
-    window.removeEventListener("resize", this.onResize, { passive: true });
-  },
-
-  mounted() {
-    this.onResize();
-
-    window.addEventListener("resize", this.onResize, { passive: true });
-  },
-  methods: {
-    onResize() {
-      this.isMobile = window.innerWidth < 600;
-    },
-  },
 };
 </script>
 
