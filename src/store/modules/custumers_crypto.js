bb51686d4e61309d010f223165759d37eb19fb8a
rukkiecodes <rukkiecodes@gmail.com>
2021-07-10 00:20:02 +0100

    added state for custumers crypto

--- src/store/modules/custumers_crypto.js
+++ src/store/modules/custumers_crypto.js
@@ -1,7 +1,19 @@
 import router from "../../router/index";
 import snackbar_state from "./snackbar_state";
 export default {
-    state: {},
+    state: {
+        custumer: {
+            track_1: false,
+            track_2: false,
+            track_3: false,
+            track_4: false,
+            track_5: false,
+            track_6: false,
+            track_7: false,
+            activeate_back_button: false,
+            activeate_next_button: false,
+        }
+    },
     mutations: {},
     actions: {},
     getters: {},
