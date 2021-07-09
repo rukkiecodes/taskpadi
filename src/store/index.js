533f1cf42bd5a2af092b4578327a40b1b987c45c
rukkiecodes <rukkiecodes@gmail.com>
2021-07-10 00:16:33 +0100

    added state for custumers crypto

--- src/store/index.js
+++ src/store/index.js
@@ -5,23 +5,25 @@ import good_and_product_buyer from "./modules/good_and_product_buyer";
 import snackbar_state from "./modules/snackbar_state";
 import delivery_info from "./modules/delivery_info"
 import good_and_product_seller from "./modules/good_and _product_seller";
+import custumers_crypty from "./modules/custumers_crypto"
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    app_nav_title: "",
-  },
-  actions: {
-    app_bar_route() {
-      this.state.app_nav_title = location.pathname;
-    }
-  },
-  modules: {
-    app_drawer,
-    good_and_product_buyer,
-    snackbar_state,
-    delivery_info,
-    good_and_product_seller
-  },
+    state: {
+        app_nav_title: "",
+    },
+    actions: {
+        app_bar_route() {
+            this.state.app_nav_title = location.pathname;
+        }
+    },
+    modules: {
+        app_drawer,
+        good_and_product_buyer,
+        snackbar_state,
+        delivery_info,
+        good_and_product_seller,
+        custumers_crypty
+    },
 });
---
+++ src/store/modules/custumers_crypto.js
@@ -0,0 +1,8 @@
+import router from "../../router/index";
+import snackbar_state from "./snackbar_state";
+export default {
+    state: {},
+    mutations: {},
+    actions: {},
+    getters: {},
+};
