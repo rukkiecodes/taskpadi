e261de80106093a6076aee64484566f79f624d1d
rukkiecodes <rukkiecodes@gmail.com>
2021-07-29 02:11:56 +0100

    a little modification

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <v-app>
     <v-main>
-      <router-view />
+      <transition name="fade" mode="out-in">
+        <router-view />
+      </transition>
     </v-main>
   </v-app>
 </template>
@@ -11,3 +13,17 @@ export default {
   name: "App",
 };
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease-in-out;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -64,25 +64,9 @@ export default {
   height: 100vh;
   overflow: auto;
 }
-.hide {
-}
 /* width */
 .hide::-webkit-scrollbar {
   width: 0px;
-}
-/* Track */
-.hide::-webkit-scrollbar-track {
-  box-shadow: inset 0 0 5px grey;
-  border-radius: 10px;
-}
-/* Handle */
-.hide::-webkit-scrollbar-thumb {
-  background: red;
-  border-radius: 10px;
-}
-/* Handle on hover */
-.hide::-webkit-scrollbar-thumb:hover {
-  background: #b30000;
 }
 
 .hide_overlay {
