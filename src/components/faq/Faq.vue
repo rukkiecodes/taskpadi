b672a7fb7617fa8446c0c89808bcc3fa94a874f2
rukkiecodes <rukkiecodes@gmail.com>
2021-08-10 00:43:52 +0100

    finished faq

--- src/components/faq/Faq.vue
+++ src/components/faq/Faq.vue
@@ -1,13 +1,15 @@
 <template>
-  <v-container fluid class="pa-0">
-    <v-container class="faq mt-0 mt-xl-0 mb-15">
-      <v-row justify="space-between" align="start">
-        <Mobile_questions />
-        <Questions />
-        <Answers />
-      </v-row>
-    </v-container>
-    <Footer />
+  <v-container id="scroll-area" fluid class="pa-0">
+    <smooth-scrollbar :options="{ damping: 0.05 }">
+      <v-container class="faq mt-0 mt-xl-0 mb-15">
+        <v-row justify="space-between" align="start">
+          <Mobile_questions />
+          <Questions />
+          <Answers />
+        </v-row>
+      </v-container>
+      <Footer />
+    </smooth-scrollbar>
   </v-container>
 </template>
 
@@ -23,10 +25,26 @@ export default {
     Answers,
     Footer,
   },
+  created() {
+    let elHtml = document.getElementsByTagName("html")[0];
+    elHtml.classList.add("hide");
+  },
 };
 </script>
 
 <style scoped>
+#scroll-area {
+  width: 100%;
+  height: 100vh;
+  overflow: auto;
+}
+.hide::-webkit-scrollbar {
+  width: 0px;
+}
+
+.hide_overlay {
+  z-index: 0 !important;
+}
 .faq {
   height: 90vh;
 }
