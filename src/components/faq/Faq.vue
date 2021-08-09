1f26bc23d1eac13dea25c127989c4592f4204735
rukkiecodes <rukkiecodes@gmail.com>
2021-08-09 18:11:11 +0100

    added footer to faq

--- src/components/faq/Faq.vue
+++ src/components/faq/Faq.vue
@@ -1,10 +1,13 @@
 <template>
-  <v-container class="faq mt-0 mt-xl-0">
-    <v-row justify="space-between" align="start">
-      <Mobile_questions />
-      <Questions />
-      <Answers />
-    </v-row>
+  <v-container fluid class="pa-0">
+    <v-container class="faq mt-0 mt-xl-0 mb-15">
+      <v-row justify="space-between" align="start">
+        <Mobile_questions />
+        <Questions />
+        <Answers />
+      </v-row>
+    </v-container>
+    <Footer />
   </v-container>
 </template>
 
@@ -12,11 +15,13 @@
 import Questions from "./components/Questions.vue";
 import Mobile_questions from "./components/Mobile_questions.vue";
 import Answers from "./components/Answers.vue";
+import Footer from "../Footer.vue";
 export default {
   components: {
     Questions,
     Mobile_questions,
     Answers,
+    Footer,
   },
 };
 </script>
