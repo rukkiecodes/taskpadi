f70b66b0553b8b31cb83c276af870e76c1a7165a
rukkiecodes <rukkiecodes@gmail.com>
2021-08-16 13:02:15 +0100

    done with transaction services

---
+++ src/components/admin/transactions/services/Create_transaction_button.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <v-btn
+      dark
+      depressed
+      color="blue accent-4"
+      @click="goods_products.dialog = true"
+      class="text-capitalize font-weight-bold rounded-lg"
+      height="40"
+    >
+      <v-icon class="mr-2">mdi-bank-transfer</v-icon>
+      Create Transaction
+    </v-btn>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  computed: {
+    ...mapState(["goods_products"]),
+    goods_products: {
+      get() {
+        return this.$store.state.goods_products;
+      },
+      set(new_value) {
+        this.$store.state.goods_products = new_value;
+      },
+    },
+  },
+};
+</script>
---
+++ src/components/admin/transactions/services/Services.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-container>
+    <Stats />
+    <Create_transaction_dialog />
+    <v-card flat>
+      <v-tabs v-model="tab" color="blue accent-4">
+        <v-tab active-class="font-weight-bold" class="text-capitalize"
+          >Completed</v-tab
+        >
+        <v-tab active-class="font-weight-bold" class="text-capitalize"
+          >Ongoing</v-tab
+        >
+        <v-tab active-class="font-weight-bold" class="text-capitalize"
+          >Pending</v-tab
+        >
+        <v-tab active-class="font-weight-bold" class="text-capitalize"
+          >Failed</v-tab
+        >
+        <v-tab active-class="font-weight-bold" class="text-capitalize"
+          >Canceled</v-tab
+        >
+      </v-tabs>
+
+      <v-tabs-items v-model="tab">
+        <v-tab-item>
+          <v-card flat>
+            <Completed />
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card flat>
+            <Ongoing />
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card flat>
+            <Pending />
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card flat>
+            <Failed />
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card flat>
+            <Canceled />
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+// @ts-nocheck
+import { mapActions, mapState } from "vuex";
+export default {
+  data: () => ({
+    tab: null,
+  }),
+  components: {
+    Create_transaction_dialog: () => import("./Create_transaction_dialog.vue"),
+    Completed: () => import("./components/completed/Completed.vue"),
+    Ongoing: () => import("./components/ongoing/Ongoing.vue"),
+    Pending: () => import("./components/pending/Pending.vue"),
+    Failed: () => import("./components/failed/Failed.vue"),
+    Canceled: () => import("./components/canceled/Canceled.vue"),
+    Stats: () => import("./Stats.vue"),
+  },
+  mounted() {
+    this.check_dash_active_route();
+    this.default_route();
+    this.my_logs();
+  },
+  methods: {
+    ...mapActions(["check_dash_active_route"]),
+    my_logs() {
+      console.log("services route: ", this.dash_nav.active_dash_route);
+    },
+    default_route() {
+      this.dash_nav.active_dash_route = "/admin/services";
+    },
+  },
+
+  computed: {
+    ...mapState(["dash_nav"]),
+    dash_nav: {
+      get() {
+        return this.$store.state.dash_nav;
+      },
+      set(new_value) {
+        this.$store.state.dash_nav = new_value;
+      },
+    },
+  },
+};
+</script>
