99a4320e9093ce6d23ef9786eadd640a272057b7
rukkiecodes <rukkiecodes@gmail.com>
2021-08-19 11:21:37 +0100

    styled Testimonials without stylesheet

--- src/components/home/components/Testimonials.vue
+++ src/components/home/components/Testimonials.vue
@@ -1,67 +1,69 @@
 <template>
-  <v-container class="testimonials">
-    <div class="head_text">
-      <p>Testimonials</p>
-    </div>
-    <div class="slide">
-      <v-sheet class="mx-auto sheet" elevation="0">
-        <v-slide-group v-model="model">
-          <v-slide-item v-for="(card, index) in cards" :key="index">
-            <v-card
-              width="320"
-              flat
-              class="ma-4 sheet_card rounded-xl px-6 py-12"
+  <v-container class="testimonials mt-16">
+    <v-row justify="space-between" align="start">
+      <v-col class="text-center text-heading text-sm-h6 text-md-h5" cols="12">
+        <h1>Testimonials</h1>
+      </v-col>
+    </v-row>
+    <v-row justify="space-between" align="start">
+      <v-col cols="12">
+        <v-row align="center">
+          <v-item-group
+            v-model="window"
+            class="shrink mr-6"
+            mandatory
+          >
+            <v-item
+              v-for="(card, index) in testimonials.slice(0, 5)"
+              :key="index"
+              v-slot="{ active, toggle }"
             >
-              <v-card-text class="ma-0 pa-0">
-                <div class="head mb-5">
-                  <div class="icon">
-                    <v-icon size="80">{{ card.icon }}</v-icon>
-                  </div>
-                </div>
-              </v-card-text>
-              <v-card-text class="ma-0 pa-0 texts">
-                <p class="p1 text-h6 font-weight-bold">{{ card.title }}</p>
-                <p class="p2 text-body-1 font-weight-bold mt-n5">
-                  {{ card.position }}
-                </p>
-                <p class="p3 text-body-2">{{ card.text }}</p>
-              </v-card-text>
-            </v-card>
-          </v-slide-item>
-        </v-slide-group>
-      </v-sheet>
-    </div>
+              <div>
+                <v-btn color="blue accent-4" :input-value="active" icon @click="toggle">
+                  <v-icon>mdi-record</v-icon>
+                </v-btn>
+              </div>
+            </v-item>
+          </v-item-group>
+
+          <v-col>
+            <v-window v-model="window" class="elevation-0 rounded-xl" vertical>
+              <v-window-item
+                v-for="(card, index) in testimonials.slice(0, 5)"
+                :key="index"
+                class="rounded-xl"
+              >
+                <v-card color="white" class="rounded-xl pa-4" flat>
+                  <v-card-text>
+                    <v-row class="mb-4 pa-2" align="center">
+                      <v-avatar
+                        size="40"
+                        color="blue accent-4"
+                        class="mr-4 text-uppercase white--text"
+                        v-text="card.title.slice(0, 2)"
+                      />
+                      <strong class="text-h6" v-text="card.title" />
+                    </v-row>
+
+                    <p v-text="card.position" />
+                    <p v-text="card.text" />
+                  </v-card-text>
+                </v-card>
+              </v-window-item>
+            </v-window>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
+import testimonials from "./testimonials"
 export default {
   data: () => ({
-    cards: [
-      {
-        icon: "mdi-account",
-        title: "Mrs Chidinma Okere",
-        position: "CEO Chi's apperel (Seller)",
-        text: `Anytime my potential customers dobt the genuinity of my business but really want to need the product, we use TrustPaddi and they are very reliable.`,
-      },
-      {
-        icon: "mdi-account",
-        title: "Mr David Elendu",
-        position: "Crypto Trader & Buyer",
-        text: `For all my Crypto transactions with any Crypto trader, I use TrustPaddi. I pay to TrustPaddi, I get my Crypto and then I approve payment instantly, so simple and reliable.`,
-      },
-      {
-        icon: "mdi-account",
-        title: "Mr Celestine Obinna",
-        position: "Online buyer",
-        text: `TrustPaddi givese total control over my transaction with unknown sellers, now I can comfortably buy any product I ant online without fear.`,
-      }
-    ],
-    model: null,
+    window: 0,
+    testimonials
   }),
 };
 </script>
-
-<style scoped>
-@import url(../../../assets/styles/testimonials.css);
-</style>
