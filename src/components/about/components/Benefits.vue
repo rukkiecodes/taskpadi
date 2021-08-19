0d314ab0e30b166de7e8b169fbb644b3e1f4c675
rukkiecodes <rukkiecodes@gmail.com>
2021-08-19 12:41:26 +0100

    done with about Benefits

--- src/components/about/components/Benefits.vue
+++ src/components/about/components/Benefits.vue
@@ -1,28 +1,42 @@
 <template>
-  <v-container>
+  <v-container class="mt-16">
     <v-row justify="space-between" align="start">
-      <v-col class=" py-15 d-flex justify-center align-center" cols="12">
-        <div class="head_text text-center">
-          <p class="p1">Benefits of TrustPaddi</p>
-        </div>
+      <v-col class="text-center text-heading text-sm-h6 text-md-h5" cols="12">
+        <h1>Benefits of TrustPaddi</h1>
       </v-col>
 
-      <v-col cols="12">
+      <v-col class="mt-8" cols="12">
         <v-row justify="space-between" align="start">
-          <v-col v-for="(benefit, i) in benefits" :key="i" cols="12" sm="12" md="4">
+          <v-col
+            v-for="(benefit, i) in benefits"
+            :key="i"
+            cols="12"
+            sm="12"
+            md="4"
+          >
             <v-row justify="start" align="start">
               <v-col
                 class="my-0 py-0 d-flex justify-start align-center"
                 cols="12"
               >
-                <span style="color: #4169e2" class="text-h6 text-bold" v-text="benefit.head" />
+                <span
+                  style="color: #4169e2"
+                  class="text-h6 text-bold"
+                  v-text="benefit.head"
+                />
               </v-col>
               <v-col cols="12">
                 <div class="d-flex flex-column">
-                  <div v-for="(text, i) in benefit.texts" :key="i" class="mb-4 benefit_text d-flex justify-start align-start">
-                    <div class="mr-2 mt-1"></div>
-                    <span class="text-body-2 grey--text text--darken-4" v-text="text.span" />
-                  </div>
+                  <p
+                    class="grey--text text--darken-4"
+                    v-for="(text, index) in benefit.texts"
+                    :key="index"
+                  >
+                    <v-icon color="blue accent-4" size="16"
+                      >mdi-radiobox-marked</v-icon
+                    >
+                    {{ text.span }}
+                  </p>
                 </div>
               </v-col>
             </v-row>
@@ -34,19 +48,10 @@
 </template>
 
 <script>
-import benefits from "./benefits.js"
+import benefits from "./benefits.js";
 export default {
   data: () => ({
-    benefits
+    benefits,
   }),
 };
 </script>
-
-<style scoped>
-.benefit_text div {
-  width: 10px;
-  height: 10px;
-  border-radius: 50%;
-  background: #4169e2;
-}
-</style>
