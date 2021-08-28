0c38c25ad4c1b367a9cbaa15974ffb5631a433d7
rukkiecodes <rukkiecodes@gmail.com>
2021-08-28 23:13:26 +0100

    done with faq

--- src/components/faq/Faq.vue
+++ src/components/faq/Faq.vue
@@ -1,21 +1,54 @@
 <template>
-  <v-container fluid class="pa-0">
-    <!-- <Head /> -->
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti quisquam
-    incidunt excepturi ex rem facere possimus deleniti tempora velit magnam a
-    culpa tenetur vero laborum nam, animi, aliquid provident est. Lorem ipsum,
-    dolor sit amet consectetur adipisicing elit. Corrupti quisquam incidunt
-    excepturi ex rem facere possimus deleniti tempora velit magnam a culpa
-    tenetur vero laborum nam, animi, aliquid provident est. Lorem ipsum, dolor
-    sit amet consectetur adipisicing elit. Corrupti quisquam incidunt excepturi
-    ex rem facere possimus deleniti tempora velit magnam a culpa tenetur vero
-    laborum nam, animi, aliquid provident est.
+  <v-container>
+    <v-row justify="space-between" align="start">
+      <v-col class="hidden-md-and-up" id="scrollArea" cols="12">
+        <MobileQuestions />
+      </v-col>
+      <v-col class="hidden-sm-and-down" id="scrollArea" cols="12" md="4">
+        <Questions />
+      </v-col>
+      <v-col cols="12" md="8">
+        <v-sheet
+          class="d-flex flex-column justify-space-between align-end"
+          :height="height"
+        >
+          <Answers />
+          <AskQuestion />
+        </v-sheet>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
   components: {
+    Questions: () => import("./components/Questions.vue"),
+    Answers: () => import("./components/Answers.vue"),
+    MobileQuestions: () => import("./components/MobileQuestions.vue"),
+    AskQuestion: () => import("./components/AskQuestion.vue"),
+  },
+  computed: {
+    height() {
+      switch (this.$vuetify.breakpoint.name) {
+        case "xs":
+          return "80vh";
+        case "sm":
+          return "80vh";
+        case "md":
+          return "100vh";
+        case "lg":
+          return "100vh";
+        case "xl":
+          return "100vh";
+      }
+    },
   },
 };
 </script>
+
+<style lang="sass" scoped>
+#scrollArea
+  width: 100%
+  overflow: auto
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import nav from "./modules/nav";
+import faq from "./modules/faq"
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     nav,
+    faq
   },
 });
