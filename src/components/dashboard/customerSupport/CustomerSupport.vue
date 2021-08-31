7ab86ec8ca488bea502f53a4b6d498f6922e5c9b
rukkiecodes <rukkiecodes@gmail.com>
2021-08-31 19:18:52 +0100

    done with dashboard

--- src/components/dashboard/customerSupport/CustomerSupport.vue
+++ src/components/dashboard/customerSupport/CustomerSupport.vue
@@ -1,9 +1,18 @@
 <template>
   <v-container>
-    <h1 class="text-h1">Customer support</h1>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, ratione quis
-    laboriosam impedit nam animi! Minima quaerat deleniti praesentium, officiis
-    laudantium optio ratione assumenda repellat soluta beatae accusamus fugit.
-    Similique!
+    <v-row justify="space-between" align="start">
+      <Form />
+      <Table />
+    </v-row>
   </v-container>
 </template>
+
+<script>
+import Table from "./components/Table.vue"
+import Form from "./components/Form.vue"
+export default {
+  components: {
+    Table,Form
+  }
+}
+</script>
---
+++ src/components/dashboard/customerSupport/components/data/header.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    text: "Ticket ID",
+    align: "start",
+    sortable: true,
+    value: "ticketId",
+  },
+  { text: "Amount refunded", value: "amountRefunded" },
+  { text: "Amount", value: "amount" },
+  { text: "Date", value: "date" },
+  { text: "Status", value: "status" },
+  { text: "View", value: "productAction", sortable: false },
+];
