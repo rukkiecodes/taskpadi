6def8e8c2e65c644950bc9b64e805e723e8a71a5
rukkiecodes <rukkiecodes@gmail.com>
2021-10-11 18:37:36 +0100

    redesigned testimpneis

--- src/components/home/components/Testimonials.vue
+++ src/components/home/components/Testimonials.vue
@@ -5,25 +5,50 @@
       src="../../../assets/trust/home/map.png"
       class="f-flex justify-center align-center"
     >
-      <v-row justify="space-around" align="center">
-        <Testimony1 />
-        <Testimony2 />
-        <Testimony3 />
-        <Testimony4 />
-        <Testimony5 />
-      </v-row>
+      <v-sheet
+        class="mx-auto groupSheet"
+        elevation="0"
+        color="transparent"
+        :max-width="groupSheetWidth"
+      >
+        <v-slide-group class="py-4" show-arrows>
+          <v-slide-item class="mr-4" v-for="(testimony, i) in testimonies" :key="i">
+            <v-card class="rounded-lg my-2" color="white" width="300">
+              <v-toolbar flat>
+                <v-avatar class="mr-3">
+                  <v-img :src="testimony.avatar" />
+                </v-avatar>
+                <v-sheet class="d-flex flex-column align-start justify-center">
+                  <v-toolbar-title v-text="testimony.name" />
+                  <span class="text-caption grey--text text--darken-3" v-text="testimony.position" />
+                </v-sheet>
+              </v-toolbar>
+              <v-card-text>
+                <v-icon color="#210070" size="30">mdi-format-quote-open</v-icon>
+                {{ testimony.body }}
+              </v-card-text>
+            </v-card>
+          </v-slide-item>
+        </v-slide-group>
+      </v-sheet>
     </v-img>
   </v-container>
 </template>
 
 <script>
+// @ts-nocheck
+import testimonies from "./testimonies"
 export default {
-  components: {
-    Testimony1: () => import("./testimony/Testimony1.vue"),
-    Testimony2: () => import("./testimony/Testimony2.vue"),
-    Testimony3: () => import("./testimony/Testimony3.vue"),
-    Testimony4: () => import("./testimony/Testimony4.vue"),
-    Testimony5: () => import("./testimony/Testimony5.vue"),
+  data: () => ({
+    groupSheetWidth: null,
+    groupSheetHeight: null,
+    testimonies
+  }),
+  mounted() {
+    const groupSheet = document.querySelector(".groupSheet")
+
+    this.groupSheetWidth = groupSheet.offsetWidth
+    this.groupSheetHeight = groupSheet.offsetHeight
   },
 }
 </script>
---
+++ src/components/home/components/testimonies.js
@@ -0,0 +1,38 @@
+// @ts-nocheck
+export default [
+  {
+    avatar: require("../../../assets/trust/home/testimony1.png"),
+    name: "Mrs Chidinma Okere",
+    position: "CEO Chi's apperel",
+    body:
+      "Anytime my potential customers dobt the genuinity of my business but really want to need the product, we use TrustPaddi and they are very reliable.",
+  },
+  {
+    avatar: require("../../../assets/trust/home/testimony2.png"),
+    name: "Mrs Chidinma Okere",
+    position: "CEO Chi's apperel",
+    body:
+      "Anytime my potential customers dobt the genuinity of my business but really want to need the product, we use TrustPaddi and they are very reliable.",
+  },
+  {
+    avatar: require("../../../assets/trust/home/testimony3.png"),
+    name: "Mrs Chidinma Okere",
+    position: "CEO Chi's apperel",
+    body:
+      "Anytime my potential customers dobt the genuinity of my business but really want to need the product, we use TrustPaddi and they are very reliable.",
+  },
+  {
+    avatar: require("../../../assets/trust/home/testimony4.png"),
+    name: "Mrs Chidinma Okere",
+    position: "CEO Chi's apperel",
+    body:
+      "Anytime my potential customers dobt the genuinity of my business but really want to need the product, we use TrustPaddi and they are very reliable.",
+  },
+  {
+    avatar: require("../../../assets/trust/home/testimony5.png"),
+    name: "Mrs Chidinma Okere",
+    position: "CEO Chi's apperel",
+    body:
+      "Anytime my potential customers dobt the genuinity of my business but really want to need the product, we use TrustPaddi and they are very reliable.",
+  },
+]
