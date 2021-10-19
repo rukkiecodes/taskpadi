8d85a47bfc71da93f718392e51ec4a5e803502b2
Terry Amagboro <rukkiecodes@gmail.com>
2021-10-19 23:41:15 +0100

    done adding transaction cards

--- src/components/dashboard/dashboard/components/TransactionCards.vue
+++ src/components/dashboard/dashboard/components/TransactionCards.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row>
     <v-col cols="12" sm="6">
-      <v-card color="#0096B7" flat dark>
+      <v-card min-height="190" color="#0096B7" flat dark>
         <v-toolbar color="transparent" flat dense>
           <v-toolbar-title>Current Ballance</v-toolbar-title>
           <v-spacer></v-spacer>
@@ -11,8 +11,48 @@
           </v-chip>
         </v-toolbar>
 
-        <v-card-text class="text-h4 white--text font-weight-bold">₦1,200,000.90</v-card-text>
-        <v-card-text class="text-subtitle-2 white--text">Ledger Balance : $1,229</v-card-text>
+        <v-card-text class="text-h4 white--text font-weight-bold"
+          >₦1,200,000.90</v-card-text
+        >
+        <v-card-text class="text-subtitle-2 white--text"
+          >Ledger Balance : $1,229</v-card-text
+        >
+      </v-card>
+    </v-col>
+    <v-col cols="12" sm="6">
+      <v-card min-height="190" color="#fff" flat>
+        <v-toolbar color="transparent" flat dense>
+          <v-toolbar-title>Trust Balance</v-toolbar-title>
+          <v-spacer></v-spacer>
+        </v-toolbar>
+
+        <v-card-text class="text-h4 grey--text text-darken-4 font-weight-bold"
+          >₦380,000.90</v-card-text
+        >
+      </v-card>
+    </v-col>
+    <v-col cols="12" sm="6">
+      <v-card min-height="100" flat>
+        <v-toolbar color="transparent" flat dense>
+          <v-toolbar-title>Total transactions</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-chip color="grey lighten-3" small>
+            <span class="text-body-2 grey--text text--darken-3">Month</span>
+            <v-icon class="grey--text text--darken-3" small right>mdi-chevron-down</v-icon>
+          </v-chip>
+        </v-toolbar>
+      </v-card>
+    </v-col>
+    <v-col cols="12" sm="6">
+      <v-card min-height="100" flat>
+        <v-toolbar color="transparent" flat dense>
+          <v-toolbar-title>Total withdrawals</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-chip color="grey lighten-3" small>
+            <span class="text-body-2 grey--text text--darken-3">Month</span>
+            <v-icon class="grey--text text--darken-3" small right>mdi-chevron-down</v-icon>
+          </v-chip>
+        </v-toolbar>
       </v-card>
     </v-col>
   </v-row>
@@ -21,5 +61,3 @@
 <script>
 export default {}
 </script>
-
-<style></style>
