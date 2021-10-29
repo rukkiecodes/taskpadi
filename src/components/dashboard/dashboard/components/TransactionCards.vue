4a8185d017fb96cf143caac24c27cb560d22cec8
Terry Amagboro <rukkiecodes@gmail.com>
2021-10-29 16:12:33 +0100

    moved Total transactions and Total withdrawals

--- src/components/dashboard/dashboard/components/TransactionCards.vue
+++ src/components/dashboard/dashboard/components/TransactionCards.vue
@@ -31,29 +31,5 @@
         >
       </v-card>
     </v-col>
-    <v-col cols="12" sm="6">
-      <v-card min-height="100" flat>
-        <v-toolbar color="transparent" flat dense>
-          <v-toolbar-title>Total transactions</v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-chip color="grey lighten-3" small>
-            <span class="text-body-2 grey--text text--darken-3">Month</span>
-            <v-icon class="grey--text text--darken-3" small right>mdi-chevron-down</v-icon>
-          </v-chip>
-        </v-toolbar>
-      </v-card>
-    </v-col>
-    <v-col cols="12" sm="6">
-      <v-card min-height="100" flat>
-        <v-toolbar color="transparent" flat dense>
-          <v-toolbar-title>Total withdrawals</v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-chip color="grey lighten-3" small>
-            <span class="text-body-2 grey--text text--darken-3">Month</span>
-            <v-icon class="grey--text text--darken-3" small right>mdi-chevron-down</v-icon>
-          </v-chip>
-        </v-toolbar>
-      </v-card>
-    </v-col>
   </v-row>
 </template>
--- src/components/dashboard/withdraws/Withdraws.vue
+++ src/components/dashboard/withdraws/Withdraws.vue
@@ -3,6 +3,7 @@
     <v-row justify="space-between" align="start">
       <Ballance />
       <Form />
+      <BallanceCards />
       <History />
     </v-row>
   </v-container>
@@ -11,12 +12,14 @@
 <script>
 import Form from "./components/Form.vue";
 import Ballance from "./components/Ballance.vue"
+import BallanceCards from "./components/BallanceCards.vue"
 import History from "./components/history/History.vue"
 export default {
   components: {
     Form,
     Ballance,
-    History
+    BallanceCards,
+    History,
   },
 };
 </script>
