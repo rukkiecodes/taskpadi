c557b65367f3771fbe075e562edac7778a820772
Terry Amagboro <rukkiecodes@gmail.com>
2021-11-19 21:41:59 +0100

    done with footed

--- src/components/dashboard/paddiPay/PaddiPay.vue
+++ src/components/dashboard/paddiPay/PaddiPay.vue
@@ -1,22 +1,39 @@
 <template>
   <v-container>
-    <v-row justify="start" align="start">
+    <v-row :class="rowClass" justify="start" align="start">
       <ProductImages />
       <ProductDetails />
     </v-row>
-    <!-- <Footer /> -->
+    <Footer />
     <Form />
   </v-container>
 </template>
 
 <script>
-import Form from './components/Form.vue'
+import Form from "./components/Form.vue"
 export default {
   components: {
     ProductImages: () => import("./components/ProductImages.vue"),
     ProductDetails: () => import("./components/ProductDetails.vue"),
     Form,
     Footer: () => import("./components/Footer.vue"),
+  },
+
+  computed: {
+    rowClass() {
+      switch (this.$vuetify.breakpoint.name) {
+        case "xs":
+          return "mb-16"
+        case "sm":
+          return "mb-0"
+        case "md":
+          return "mb-16"
+        case "lg":
+          return "mb-16"
+        case "xl":
+          return "mb-16"
+      }
+    },
   },
 }
 </script>
--- src/components/dashboard/paddiPay/components/Footer.vue
+++ src/components/dashboard/paddiPay/components/Footer.vue
@@ -1,10 +1,15 @@
 <template>
   <v-footer color="white" absolute>
-    <v-col
-      class="text-center"
-      cols="12"
-    >
-      <strong>Powered by TrustPaddi <small class="ml-3 font-weight-light"><v-icon color="grey darken-3" class="mt-n1" size="15">mdi-copyright</v-icon> {{ new Date().getFullYear() }}</small></strong>
+    <v-col class="text-center" cols="12">
+      <span
+        >Powered by TrustPaddi
+        <small class="ml-3 font-weight-light"
+          ><v-icon color="grey darken-3" class="mt-n1" size="15"
+            >mdi-copyright</v-icon
+          >
+          {{ new Date().getFullYear() }}</small
+        ></span
+      >
     </v-col>
   </v-footer>
 </template>
