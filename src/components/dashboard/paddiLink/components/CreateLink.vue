67f6b94395b990216944fe439d4c7b075964f5fb
Terry Amagboro <rukkiecodes@gmail.com>
2022-01-13 02:50:49 +0100

    fixed create paddi link doaloge

--- src/components/dashboard/paddiLink/components/CreateLink.vue
+++ src/components/dashboard/paddiLink/components/CreateLink.vue
@@ -1,85 +1,120 @@
 <template>
-  <v-dialog v-model="paddiLink.dialog" scrollable>
-    <v-card max-height="500" width="400">
+  <v-dialog v-model="paddiLink.dialog" width="500" scrollable>
+    <v-card flat max-height="500">
       <v-card-title class="grey--text text--darken-4">
-        Create a Paddi link
+        Create Paddi link
         <v-spacer />
         <v-btn @click="paddiLink.dialog = false" icon>
           <v-icon>mdi-close</v-icon>
         </v-btn>
       </v-card-title>
+
       <v-card-text>
-        <v-text-field
-          color="deep-purple accent-4"
-          label="Product Name/Title"
-          v-model="paddiLink.paddiLinkInputs.title"
-          full-width
-          outlined
-          dense
-        ></v-text-field>
-        <v-text-field
-          color="deep-purple accent-4"
-          v-model="paddiLink.paddiLinkInputs.price"
-          label="Product Price"
-          full-width
-          outlined
-          dense
-        ></v-text-field>
-        <v-textarea
-          color="deep-purple accent-4"
-          label="Product Product Description"
-          :hint="
-            `Word limit of ${max} words (${max -
-              paddiLink.paddiLinkInputs.description.length} left)`
-          "
-          v-model="paddiLink.paddiLinkInputs.description"
-          :maxlength="max"
-          full-width
-          outlined
-          counter
-        ></v-textarea>
-        <v-file-input
-          full-width
-          outlined
-          dense
-          clearable
-          placeholder="upload a photo"
-          label="upload a photo"
-          @change="onFileChange"
-        ></v-file-input>
-        <v-img
-          class="mx-auto"
-          v-show="paddiLink.paddiLinkInputs.image"
-          :src="paddiLink.paddiLinkInputs.image"
-        ></v-img>
+        <v-row justify="space-between" align="start">
+          <v-col cols="12" sm="6">
+            <v-text-field
+              flat
+              solo
+              hide-details
+              class="rounded-lg"
+              label="Product title"
+              background-color="#EFF0F6"
+              v-model="paddiLink.paddiLinkInputs.title"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="6">
+            <v-text-field
+              flat
+              solo
+              hide-details
+              label="Price"
+              class="rounded-lg"
+              background-color="#EFF0F6"
+              v-model="paddiLink.paddiLinkInputs.price"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12">
+            <v-textarea
+              flat
+              solo
+              no-resize
+              full-width
+              hide-details
+              :maxlength="max"
+              class="rounded-lg"
+              background-color="#EFF0F6"
+              label="Product Product Description"
+              v-model="paddiLink.paddiLinkInputs.description"
+            ></v-textarea>
+          </v-col>
+          <v-col cols="12" sm="6">
+            <v-select
+              flat
+              solo
+              full-width
+              hide-details
+              :items="items"
+              class="rounded-lg"
+              label="Product category"
+              background-color="#EFF0F6"
+            ></v-select>
+          </v-col>
+          <v-col cols="12" sm="6">
+            <v-file-input
+              flat
+              solo
+              clearable
+              full-width
+              hide-details
+              label="Add image"
+              class="rounded-lg"
+              truncate-length="8"
+              @change="onFileChange"
+              background-color="#EFF0F6"
+            ></v-file-input>
+          </v-col>
+          <v-col cols="12">
+            <v-btn
+              dark
+              large
+              block
+              depressed
+              @click="createPaddiLink"
+              color="deep-purple accent-4"
+              class="text-body-2 font-weight-bold text-capitalize"
+            >
+              Create paddi link
+            </v-btn>
+          </v-col>
+        </v-row>
       </v-card-text>
-      <v-card-actions>
-        <v-btn
-          @click="createPaddiLink"
-          color="deep-purple accent-4"
-          dark
-          block
-          depressed
-        >
-          Done
-        </v-btn>
-      </v-card-actions>
     </v-card>
+    <vs-dialog auto-width not-padding v-model="showImageDialoge">
+      <div class="con-image">
+        <img :src="paddiLink.paddiLinkInputs.image" alt="" />
+      </div>
+    </vs-dialog>
   </v-dialog>
 </template>
 
 <script>
+// @ts-nocheck
 import { mapActions, mapState } from "vuex"
 export default {
   data: () => ({
     max: 130,
+    showImageDialoge: false,
+    items: ["Foo", "Bar", "Fizz", "Buzz"],
   }),
 
   methods: {
     onFileChange(image) {
-      if (image)
+      if (image) {
+        this.showImageDialoge = true
         this.paddiLink.paddiLinkInputs.image = URL.createObjectURL(image)
+      }
     },
+
     ...mapActions(["createPaddiLink"]),
   },
 
@@ -88,3 +123,16 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.con-image {
+  border-radius: inherit;
+}
+
+.con-image img {
+  display: block;
+  position: relative;
+  border-radius: inherit;
+  max-width: 350px;
+}
+</style>
