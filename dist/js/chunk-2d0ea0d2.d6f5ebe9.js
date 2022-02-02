a748e96bcbd218b015c4a0a10fc52715c63b5e6f
Terry Amagboro <rukkiecodes@gmail.com>
2022-02-02 09:09:51 +0100

    dist ready

---
+++ dist/js/chunk-2d0ea0d2.d6f5ebe9.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0ea0d2"],{"8fc1":function(t,e,n){"use strict";n.r(e);var l=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("h1",[t._v(t._s(t.title))])},c=[],r={props:{title:String}},s=r,u=n("2877"),i=Object(u["a"])(s,l,c,!1,null,null,null);e["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0ea0d2.d6f5ebe9.js.map
