f82f344ceaba5e823335f2b22a53d4666e07f706
Terry Amagboro <rukkiecodes@gmail.com>
2022-02-14 22:38:26 +0100

    account update now works

--- src/main.js
+++ src/main.js
@@ -9,15 +9,6 @@ import Vuesax from "vuesax"
 import "./vuesax.css"
 import AxiosPlugin from "vue-axios-cors"
 import VueCookies from "vue-cookies"
-// import VueResource from "vue-resource"
-
-// Vue.use(VueResource)
-
-// Vue.http.headers.common["Content-Type"] = "application/json"
-// Vue.http.headers.common["Access-Control-Allow-Origin"] = "*"
-// Vue.http.headers.common["Accept"] = "application/json, text/plain, */*"
-// Vue.http.headers.common["Access-Control-Allow-Headers"] =
-//   "Origin, Accept, Content-Type, Authorization, Access-Control-Allow-Origin"
 
 Vue.use(VueCookies)
 Vue.use(AxiosPlugin)
