024624522d26dcafa982da72ef52f225fe58ff95
Terry Amagboro <rukkiecodes@gmail.com>
2022-03-09 02:11:35 +0100

    signin works

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,6 @@ module.exports = {
     proxy: {
       "/api": {
         target: "https://trustpaddi.herokuapp.com",
-        // target: "http://localhost:3000",
         changeOrigin: true,
         pathRewrite: {
           "^/api": "",
