9729f98d764b0700e0d641524eeb81ca2aca757d
Terry Amagboro <rukkiecodes@gmail.com>
2022-03-25 03:04:25 +0100

    update product

--- server/routes/product/updateProduct.js
+++ server/routes/product/updateProduct.js
@@ -1,4 +1,5 @@
 const router = require("express").Router()
+const fs = require("fs")
 const { image } = require(".././../middleware/multer")
 const checkAuth = require("../../middleware/checkAuth")
 
@@ -10,53 +11,67 @@ router.post("/updateProduct", image, async(req, res) => {
     let charge = (20 / 100) * price
     let total = Number(charge) + Number(price)
 
-    try {
-        let product
-        if (req.file) {
-            product = await Product.updateOne({
-                $and: [{ user }, { _id }],
-            }, {
-                $set: {
-                    name,
-                    description,
-                    quantity,
-                    price,
-                    charge,
-                    total,
-                    image: req.file.path,
-                },
-            }).exec()
-            res.status(200).json({
-                product,
-                success: true,
-                message: "Product successfully updated",
-            })
-        } else {
-            product = await Product.updateOne({
-                $and: [{ user }, { _id }],
-            }, {
-                $set: {
-                    name,
-                    description,
-                    quantity,
-                    price,
-                    charge,
-                    total,
-                },
-            }).exec()
-            res.status(200).json({
-                Product,
-                success: true,
-                message: "Product successfully updated",
+    const updateProduct = async() => {
+        try {
+            let product
+            if (req.file) {
+                product = await Product.updateOne({
+                    $and: [{ user }, { _id }],
+                }, {
+                    $set: {
+                        name,
+                        description,
+                        quantity,
+                        price,
+                        charge,
+                        total,
+                        image: req.file.path,
+                    },
+                }).exec()
+                res.status(200).json({
+                    product,
+                    success: true,
+                    message: "Product successfully updated",
+                })
+            } else {
+                product = await Product.updateOne({
+                    $and: [{ user }, { _id }],
+                }, {
+                    $set: {
+                        name,
+                        description,
+                        quantity,
+                        price,
+                        charge,
+                        total,
+                    },
+                }).exec()
+                res.status(200).json({
+                    Product,
+                    success: true,
+                    message: "Product successfully updated",
+                })
+            }
+        } catch (error) {
+            return res.status(401).json({
+                error,
+                success: false,
+                message: "Error updating product",
             })
         }
-    } catch (error) {
-        return res.status(401).json({
-            error,
-            success: false,
-            message: "Error updating product",
-        })
     }
+
+    Product.findOne({ $and: [{ user }, { _id }] }).then((image) => {
+        let productImage
+        if (image.image) productImage = `./${image.image}`
+
+        try {
+            if (productImage) fs.unlinkSync(productImage)
+            updateProduct()
+        } catch (err) {
+            updateProduct()
+        }
+    })
 })
 
 module.exports = router
